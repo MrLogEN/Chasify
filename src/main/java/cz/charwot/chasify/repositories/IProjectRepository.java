eac98c9e27f45becdcc9ed272d655659c0a36a10
Vilém Charwot <vilem.charwot@proton.me>
2025-06-18 17:30:39 +0200

    Implement ProjectRepository

---
+++ src/main/java/cz/charwot/chasify/repositories/IProjectRepository.java
@@ -0,0 +1,82 @@
+package cz.charwot.chasify.repositories;
+
+import cz.charwot.chasify.models.Project;
+import cz.charwot.chasify.models.User;
+
+import java.util.List;
+
+public interface IProjectRepository {
+
+       /**
+     * Persists a new project.
+     * @param project the project to create
+     */
+    void create(Project project);
+
+    /**
+     * Updates an existing project.
+     * @param project the project to update
+     * @throws IllegalArgumentException if the project does not exist
+     */
+    void update(Project project);
+
+    /**
+     * Deletes the given project.
+     * @param project the project to delete
+     */
+    void delete(Project project);
+
+    /**
+     * Deletes a project by its ID.
+     * @param id the ID of the project
+     */
+    void deleteById(Long id);
+
+    /**
+     * Finds a project by its ID.
+     * @param id the ID of the project
+     * @return the matching project or null if not found
+     */
+    Project findById(Long id);
+
+    /**
+     * Returns all projects in the system.
+     * @return a list of all projects
+     */
+    List<Project> findAll();
+
+    /**
+     * Returns all projects owned by a specific user.
+     * @param owner the user who owns the projects
+     * @return a list of projects owned by the user
+     */
+    List<Project> findByOwner(User owner);
+
+    /**
+     * Returns all projects where the user is a participant.
+     * @param user the user who is part of the project
+     * @return a list of associated projects
+     */
+    List<Project> findByUser(User user);
+
+    /**
+     * Adds a user to the specified project.
+     * @param projectId the id of a project to add a user to
+     * @param user the user to be added to the project
+     */
+    void addUserToProject(Long projectId, User user);
+
+    /**
+     * Archives a project by ID.
+     * @param id the project ID
+     * @throws IllegalArgumentException if the project does not exist
+     */
+    void archive(Long id);
+
+    /**
+     * Unarchives a project by ID.
+     * @param id the project ID
+     * @throws IllegalArgumentException if the project does not exist
+     */
+    void unarchive(Long id); 
+}
---
+++ src/main/java/cz/charwot/chasify/repositories/ProjectRepository.java
@@ -0,0 +1,243 @@
+package cz.charwot.chasify.repositories;
+
+import org.slf4j.LoggerFactory;
+
+import cz.charwot.chasify.models.Project;
+import cz.charwot.chasify.models.User;
+
+import org.hibernate.dialect.function.AvgFunction.ReturnTypeResolver;
+import org.hibernate.resource.beans.internal.FallbackBeanInstanceProducer;
+import org.slf4j.Logger;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+public class ProjectRepository implements IProjectRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProjectRepository.class);
+
+    private final EntityManagerFactory emf;
+
+    public ProjectRepository(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public void create(Project project) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.persist(project);
+            tx.commit();
+            logger.debug("Project created: {}", project);
+        }
+        catch (Exception e) {
+            if(tx.isActive()) {
+                tx.rollback();
+            }
+            logger.error("Failed to create project!", e);
+            throw e;
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void update(Project project) throws IllegalArgumentException {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Project existing = em.find(Project.class, project.getId());
+            if(existing == null) {
+                throw new IllegalArgumentException("Cannot update project: project does not exist!");
+            }
+            em.merge(project);
+            tx.commit();
+            logger.debug("Project created: {}", project);
+        }
+        catch (Exception e) {
+            if(tx.isActive()) {
+                tx.rollback();
+            }
+            logger.error("Failed to update project!", e);
+            throw e;
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void delete(Project project) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Project attachedProject = em.contains(project) ? project : em.merge(project);
+            em.remove(attachedProject);
+            em.merge(project);
+            tx.commit();
+            logger.debug("Project deleted: {}", project);
+        }
+        catch (Exception e) {
+            if(tx.isActive()) {
+                tx.rollback();
+            }
+            logger.error("Failed to delete project!", e);
+            throw e;
+        }
+        finally {
+            em.close();
+        }
+
+    }
+
+    @Override
+    public void deleteById(Long id) throws IllegalArgumentException {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Project existing = em.find(Project.class, id);
+            if(existing == null) {
+                throw new IllegalArgumentException("Cannot delete project: project does not exist!");
+            }
+            tx.commit();
+            logger.debug("Project deleted by ID: {}, Project: {}", id, existing);
+        }
+        catch (Exception e) {
+            if(tx.isActive()) {
+                tx.rollback();
+            }
+            logger.error("Failed to delete project!", e);
+            throw e;
+        }
+        finally {
+            em.close();
+        }
+
+    }
+
+    @Override
+    public Project findById(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(Project.class, id);
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Project> findAll() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.createQuery("SELECT p FROM Project p", Project.class).getResultList();
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Project> findByOwner(User owner) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.createQuery("SELECT p FROM Project p WHERE p.owner = :owner", Project.class)
+            .setParameter("owner", owner)
+            .getResultList();
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Project> findByUser(User user) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.createQuery("SELECT p FROM Project p JOIN p.users u WHERE u = :user", Project.class)
+            .setParameter("user", user)
+            .getResultList();
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void addUserToProject(Long projectId, User user) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+
+            Project project = em.find(Project.class, projectId);
+            if (project == null) {
+                throw new IllegalArgumentException("Project not found with ID: " + projectId);
+            }
+
+            User managedUser = em.find(User.class, user.getId());
+            if (managedUser == null) {
+                throw new IllegalArgumentException("User not found with ID: " + user.getId());
+            }
+
+            if (!project.getUsers().contains(managedUser)) {
+                project.getUsers().add(managedUser);
+            }
+
+            tx.commit();
+            logger.debug("User {} added to project {}", user.getId(), projectId);
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            logger.error("Failed to add user {} to project {}", user.getId(), projectId, e);
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void archive(Long id) {
+        toggleArchive(id, true);
+    }
+
+    @Override
+    public void unarchive(Long id) {
+        toggleArchive(id, false);
+    }
+
+    private void toggleArchive(Long id, boolean archived) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            Project project = em.find(Project.class, id);
+            if (project == null) {
+                throw new IllegalArgumentException("Project not found with ID: " + id);
+            }
+            project.setArchived(archived);
+            tx.commit();
+            logger.debug("Project {}archived: {}", archived ? "" : "un", project);
+        }
+        catch (Exception e) {
+            if(tx.isActive()) {
+                tx.rollback();
+            }
+            logger.error("Failed to (un)archive  project!", e);
+            throw e;
+        }
+        finally {
+            em.close();
+        }
+    }
+
+}
